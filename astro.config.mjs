137d7ff79d20e8aff933c2673572bda134f53f78
AmeMeida <ame.pistache@gmail.com>
2025-10-04 04:13:09 -0300

    quase terminei a landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import db from "@astrojs/db";
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -16,8 +16,27 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  image: {
+    domains: ["picsum.photos"]
+  },
   prefetch: {
     defaultStrategy: "hover",
     prefetchAll: true
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Zen Dots",
+        cssVariable: "--font-title"
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Kanit",
+        weights: [400, 500, 600, 700],
+        styles: ["normal", "italic"],
+        cssVariable: "--font-text"
+      }
+    ]
   }
 });
