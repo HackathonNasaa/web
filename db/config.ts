ac14f5dcec595cf22f75df1847d910b4e81bd0b5
AmeMeida <ame.pistache@gmail.com>
2025-10-04 10:55:02 -0300

    nao sei oq ta acontecendo mais

--- db/config.ts
+++ db/config.ts
@@ -2,5 +2,7 @@ import { defineDb } from "astro:db";
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+
+  }
 });
--- src/kv.ts
+++ src/kv.ts
@@ -1,6 +1,18 @@
+import { createDatabase } from "db0";
+import libSql from "db0/connectors/libsql/web";
 import { createStorage } from "unstorage";
+import dbDriver from "unstorage/drivers/db0";
 import fsDriver from "unstorage/drivers/fs";
 
+const database = import.meta.env.PROD
+  ? createDatabase(
+      libSql({
+        url: import.meta.env.ASTRO_DB_REMOTE_URL,
+        authToken: import.meta.env.ASTRO_DB_APP_TOKEN
+      })
+    )
+  : undefined;
+
 export default createStorage({
-  driver: fsDriver({ base: "./.tmp" })
+  driver: database ? dbDriver({ database }) : fsDriver({ base: "./.tmp" })
 });
