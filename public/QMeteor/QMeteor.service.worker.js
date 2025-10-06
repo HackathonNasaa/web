4661e87ab3d59459afd9e5adcc122ed63d28622b
AmeMeida <ame.pistache@gmail.com>
2025-10-05 22:26:18 -0300

    NOVA VERSAO JOGO

--- public/QMeteor/QMeteor.service.worker.js
+++ public/QMeteor/QMeteor.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759709568|59706150';
+const CACHE_VERSION = '1759713936|82090721';
 /** @type {string} */
 const CACHE_PREFIX = 'Projet: Siderali-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
